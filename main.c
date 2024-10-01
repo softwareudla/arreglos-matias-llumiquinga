9174a3b8170d1f0e95b41266046ed54ff559477d
matias.llumiquinga <matias.llumiquinga@udla.edu.ec>
2024-10-01 05:54:22 -0500

    ingreso de estudiantes

--- main.c
+++ main.c
@@ -1,6 +1,38 @@
 #include <stdio.h>
+#include <string.h>
 
 int main (int argc, char *argv[]) 
 {
+    int longitud;
+    char estudiantes[5][21];
+
+    for (int i = 0; i < 2; i++)
+    {
+        fflush(stdin);
+        printf("Ingrese el nombre y apellido del estudiante %d:\n", i+1);
+        fgets(estudiantes[i], 21, stdin);
+        
+        longitud = strlen(estudiantes[i]);
+
+        if (estudiantes[i][longitud-1] != '\n')
+        {
+            printf("Se ha superado el máximo de 20 caracteres. Ingrese solo el nombre del estudiante: %d\n", i+1);
+            i--;
+
+            // Limpiar el buffer de entrada si el usuario ingresó más de los caracteres permitidos
+            int c;
+            while ((c = getchar()) != '\n' && c != EOF); // Vaciar el buffer de entrada
+        }
+
+
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%s\n", estudiantes[i]);
+    }
+    
+    
+
     return 0;
 }
