1e43669f308296b65bbfad96d3702e0816772418
matias.llumiquinga <matias.llumiquinga@udla.edu.ec>
2024-10-01 19:55:52 -0500

    ingreso de materias

--- main.c
+++ main.c
@@ -3,13 +3,14 @@
 
 int main (int argc, char *argv[]) 
 {
-    const int numEstudiantes = 5, espacioCadena = 21;
+    const int numEstudiantes = 5, numMaterias = 3, espacioCadena = 21;
     int longitudCadena, limpiadorBuffer;
 
-    char estudiantes[numEstudiantes][espacioCadena];
+    char estudiantes[numEstudiantes][espacioCadena], materias[numMaterias][espacioCadena];
 
 
-    printf("------------------------REGISTRO DE NOMBRES DE ESTUDIANTES------------------------\n");
+    printf("----------------------------- REGISTRO DE ESTUDIANTES ----------------------------\n");
+
 
     for (int i = 0; i < numEstudiantes; i++)
     {
@@ -28,17 +29,43 @@ int main (int argc, char *argv[])
         }else{
             estudiantes[i][longitudCadena-1] = '\0';
         }
+    }
 
+    printf("------------------------------ REGISTRO DE MATERIAS ------------------------------\n");
+
+    for (int i = 0; i < numMaterias; i++)
+    {
+        printf("Ingrese el nombre de la materia %d (MAX 20 caracteres):\n", i+1);
+        fflush(stdin);
+        fgets(materias[i], espacioCadena+1, stdin);
+        
+        longitudCadena = strlen(materias[i]);
+
+        if (materias[i][longitudCadena-1] != '\n')
+        {
+            printf("Se ha superado el maximo de 20 caracteres\n");
+            i--;
+
+            while ((limpiadorBuffer = getchar()) != '\n' && limpiadorBuffer != EOF);
+        }else{
+            materias[i][longitudCadena-1] = '\0';
+        }
     }
     
 
 
 
-    for (int i = 0; i < 5; i++)
+
+    
+    for (int i = 0; i < numEstudiantes; i++)
     {
         printf("%s\n", estudiantes[i]);
     }
     
+    for (int i = 0; i < numMaterias; i++)
+    {
+        printf("%s\n", materias[i]);
+    }
     
 
     return 0;
