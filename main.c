07cd6b1e6c1f359303f3c7eb96fb77bbda2794bb
matias.llumiquinga <matias.llumiquinga@udla.edu.ec>
2024-10-03 09:25:40 -0500

    Alumnos aprobados-reprobados

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 
-#define NUM_ESTUDIANTES 2
-#define NUM_MATERIAS 2
-#define NUM_CALIF 6
+#define NUM_ESTUDIANTES 5
+#define NUM_MATERIAS 3
+#define NUM_CALIF 5
 #define ESPACIO_CADENA 21
 
 
@@ -15,7 +15,7 @@ void CalcularPromedios( float promGeneral[NUM_ESTUDIANTES], int numEst,
                         float promMateriasEst[NUM_MATERIAS],
                         float califEst[NUM_CALIF][NUM_MATERIAS]);
 
-void CalcularMinMax(    float califEst[NUM_CALIF][NUM_MATERIAS],
+void CalcularMinMax(    float califEst[NUM_CALIF][NUM_MATERIAS], int numEst,
                         float califMinEst[NUM_MATERIAS],
                         float califMaxEst[NUM_MATERIAS],
                         float calfMinGeneral[NUM_ESTUDIANTES],
@@ -37,7 +37,7 @@ int main (int argc, char *argv[])
 {
     char estudiantes[NUM_ESTUDIANTES][ESPACIO_CADENA], materias[NUM_MATERIAS][ESPACIO_CADENA];
 
-    int longitudCadena, limpiadorBuffer;
+    int longitudCadena, limpiadorBuffer, aprobados=0, reprobados=0;
 
     float   promGenerales[NUM_ESTUDIANTES], califMinGenerales[NUM_ESTUDIANTES], califMaxGenenerales[NUM_ESTUDIANTES];
 
@@ -59,17 +59,50 @@ int main (int argc, char *argv[])
             
 
 
-
+    for (int i = 0; i < NUM_CALIF; i++)
+    {
+        for (int j = 0; j < NUM_MATERIAS; j++)
+        {
+            califEst1[i][j]=0;
+            califEst2[i][j]=0;
+            califEst3[i][j]=0;
+            califEst4[i][j]=0;
+            califEst5[i][j]=0;
+        }
+        
+    }
+    
 
     for (int i = 0; i < NUM_MATERIAS; i++)
     {
         promMateriasEst1[i] = 0;
         califMinEst1[i] = 10;
         califMaxEst1[i] = 0;
+        
+        promMateriasEst2[i] = 0;
+        califMinEst2[i] = 10;
+        califMaxEst2[i] = 0;
+
+        promMateriasEst3[i] = 0;
+        califMinEst3[i] = 10;
+        califMaxEst3[i] = 0;
+
+        promMateriasEst4[i] = 0;
+        califMinEst4[i] = 10;
+        califMaxEst4[i] = 0;
+
+        promMateriasEst5[i] = 0;
+        califMinEst5[i] = 10;
+        califMaxEst5[i] = 0;
+    }
+
+    for (int i = 0; i < NUM_ESTUDIANTES; i++)
+    {
         califMinGenerales[i] = 10;
         califMaxGenenerales[i] = 0;
         promGenerales[i] = 0;
     }
+    
 
     printf("----------------------------- REGISTRO DE ESTUDIANTES ----------------------------\n");
 
@@ -118,13 +151,74 @@ int main (int argc, char *argv[])
     //-------------------------------------ESTUDIANTE 1
     RegistrarCalificaciones(califEst1,0,materias, estudiantes);
     CalcularPromedios(promGenerales,0,promMateriasEst1,califEst1);
-    CalcularMinMax(califEst1, califMinEst1, califMaxEst1,califMinGenerales,califMaxGenenerales);
+    CalcularMinMax(califEst1, 0, califMinEst1, califMaxEst1,califMinGenerales,califMaxGenenerales);
+
+    //-------------------------------------ESTUDIANTE 2
+    RegistrarCalificaciones(califEst2,1,materias, estudiantes);
+    CalcularPromedios(promGenerales,1,promMateriasEst2,califEst2);
+    CalcularMinMax(califEst2, 1, califMinEst2, califMaxEst2,califMinGenerales,califMaxGenenerales);
+
+    //-------------------------------------ESTUDIANTE 3
+    RegistrarCalificaciones(califEst3,2,materias, estudiantes);
+    CalcularPromedios(promGenerales,2,promMateriasEst3,califEst3);
+    CalcularMinMax(califEst3, 2, califMinEst3, califMaxEst3,califMinGenerales,califMaxGenenerales);
+
+    //-------------------------------------ESTUDIANTE 4
+    RegistrarCalificaciones(califEst4,3,materias, estudiantes);
+    CalcularPromedios(promGenerales,3,promMateriasEst4,califEst4);
+    CalcularMinMax(califEst4, 3, califMinEst4, califMaxEst4,califMinGenerales,califMaxGenenerales);
+    
+    //-------------------------------------ESTUDIANTE 5
+    RegistrarCalificaciones(califEst5,4,materias, estudiantes);
+    CalcularPromedios(promGenerales,4,promMateriasEst5,califEst5);
+    CalcularMinMax(califEst5, 4, califMinEst5, califMaxEst5,califMinGenerales,califMaxGenenerales);
 
 
+
+
+    //-------------------------------------ESTUDIANTE 1
     ImprimirResultados( estudiantes, 0, materias, califEst1, 
                         promMateriasEst1, promGenerales, califMinEst1, 
                         califMaxEst1, califMinGenerales,califMaxGenenerales);
     
+    //-------------------------------------ESTUDIANTE 2
+    ImprimirResultados( estudiantes, 1, materias, califEst2, 
+                        promMateriasEst2, promGenerales, califMinEst2, 
+                        califMaxEst2, califMinGenerales,califMaxGenenerales);
+
+    //-------------------------------------ESTUDIANTE 3
+    ImprimirResultados( estudiantes, 2, materias, califEst3, 
+                        promMateriasEst3, promGenerales, califMinEst3, 
+                        califMaxEst3, califMinGenerales,califMaxGenenerales);
+
+    //-------------------------------------ESTUDIANTE 4
+    ImprimirResultados( estudiantes, 3, materias, califEst4, 
+                        promMateriasEst4, promGenerales, califMinEst4, 
+                        califMaxEst4, califMinGenerales,califMaxGenenerales);
+
+    //-------------------------------------ESTUDIANTE 5
+    ImprimirResultados( estudiantes, 4, materias, califEst5, 
+                        promMateriasEst5, promGenerales, califMinEst5, 
+                        califMaxEst5, califMinGenerales,califMaxGenenerales);
+    
+    for (int i = 0; i < NUM_ESTUDIANTES; i++)
+    {
+        if (promGenerales[i]>=6)
+        {
+            aprobados++;
+        }else
+        {
+            reprobados++;
+        }
+        
+    }
+
+    printf("\nAlumnos APROBADOS: %d\n",aprobados);
+    printf("Alumnos REPROBADOS: %d\n",reprobados);
+
+
+    
+
 
     return 0;
 }
@@ -135,7 +229,7 @@ void RegistrarCalificaciones(   float califEst[NUM_CALIF][NUM_MATERIAS], int num
                                 char materia[NUM_MATERIAS][ESPACIO_CADENA],
                                 char estudiant[NUM_ESTUDIANTES][ESPACIO_CADENA])
 {
-    printf("--------------------------- REGISTRO DE CALIFICACIONES DE %s\n", estudiant[0]);
+    printf("--------------------------- REGISTRO DE CALIFICACIONES DE %s\n", estudiant[numEst]);
     for (int i = 0; i < NUM_MATERIAS; i++)
     {
         printf("-------------- Ingrese las calificaciones de %s\n", materia[i]);
@@ -176,7 +270,7 @@ void CalcularPromedios( float promGeneral[NUM_ESTUDIANTES], int numEst,
 
 
 
-void CalcularMinMax(    float califEst[NUM_CALIF][NUM_MATERIAS],
+void CalcularMinMax(    float califEst[NUM_CALIF][NUM_MATERIAS], int numEst,
                         float califMinEst[NUM_MATERIAS],
                         float califMaxEst[NUM_MATERIAS],
                         float calfMinGeneral[NUM_ESTUDIANTES],
@@ -201,13 +295,13 @@ void CalcularMinMax(    float califEst[NUM_CALIF][NUM_MATERIAS],
     {
         for (int j = 0; j < NUM_CALIF; j++)
         {
-            if (califEst[j][i]<calfMinGeneral[0])
+            if (califEst[j][i]<calfMinGeneral[numEst])
             {
-                calfMinGeneral[0]=califEst[j][i];
+                calfMinGeneral[numEst]=califEst[j][i];
             }
-            if (califEst[j][i]>calfMaxGeneral[0])
+            if (califEst[j][i]>calfMaxGeneral[numEst])
             {
-                calfMaxGeneral[0]=califEst[j][i];
+                calfMaxGeneral[numEst]=califEst[j][i];
             }
         }   
     }
@@ -374,4 +468,5 @@ void ImprimirResultados(char estudiant[NUM_ESTUDIANTES][ESPACIO_CADENA], int num
             printf("-");
         }
     }
+
 }
