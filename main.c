a9f5be803f8a154660af780d36603c84b8359788
matias.llumiquinga <matias.llumiquinga@udla.edu.ec>
2024-10-02 20:46:24 -0500

    Creacion funcion CalcularMinMax

--- main.c
+++ main.c
@@ -15,6 +15,12 @@ void CalcularPromedios( float promGeneral[NUM_ESTUDIANTES], int numEst,
                         float promMateriasEst[NUM_MATERIAS],
                         float califEst[NUM_CALIF][NUM_MATERIAS]);
 
+void CalcularMinMax(    float califEst[NUM_CALIF][NUM_MATERIAS],
+                        float califMinEst[NUM_MATERIAS],
+                        float califMaxEst[NUM_MATERIAS],
+                        float calfMinGeneral[NUM_ESTUDIANTES],
+                        float calfMaxGeneral[NUM_ESTUDIANTES]);
+
 void ImprimirResultados(char estudiant[NUM_ESTUDIANTES][ESPACIO_CADENA], int numEst,
                         char materi[NUM_MATERIAS][ESPACIO_CADENA], 
                         float califiEstud[NUM_CALIF][NUM_MATERIAS],
@@ -33,12 +39,12 @@ int main (int argc, char *argv[])
 
     int longitudCadena, limpiadorBuffer;
 
+    float   promGenerales[NUM_ESTUDIANTES], califMinGenerales[NUM_ESTUDIANTES], califMaxGenenerales[NUM_ESTUDIANTES];
 
 
     float   califEst1[NUM_CALIF][NUM_MATERIAS], 
             promMateriasEst1[NUM_MATERIAS], califMinEst1[NUM_MATERIAS],califMaxEst1[NUM_MATERIAS];
             
-    float   promGenerales[NUM_ESTUDIANTES], calfMinGenerales[NUM_ESTUDIANTES], calfMaxGenenerales[NUM_ESTUDIANTES];
 
 
 
@@ -48,8 +54,8 @@ int main (int argc, char *argv[])
         promMateriasEst1[i] = 0;
         califMinEst1[i] = 10;
         califMaxEst1[i] = 0;
-        calfMinGenerales[i] = 10;
-        calfMaxGenenerales[i] = 0;
+        califMinGenerales[i] = 10;
+        califMaxGenenerales[i] = 0;
         promGenerales[i] = 0;
     }
 
@@ -102,45 +108,12 @@ int main (int argc, char *argv[])
 
     CalcularPromedios(promGenerales,0,promMateriasEst1,califEst1);
 
-
-
-
-
-    //----------------------CÁLCULO DE MIN Y MAX
-    for (int i = 0; i < NUM_MATERIAS; i++)
-    {
-        for (int j = 0; j < NUM_CALIF; j++)
-        {
-            if (califEst1[j][i]<califMinEst1[i])
-            {
-                califMinEst1[i]=califEst1[j][i];
-            }
-            if (califEst1[j][i]>califMaxEst1[i])
-            {
-                califMaxEst1[i]=califEst1[j][i];
-            }
-        }       
-    }
-
-    for (int i = 0; i < NUM_MATERIAS; i++)
-    {
-        for (int j = 0; j < NUM_CALIF; j++)
-        {
-            if (califEst1[j][i]<calfMinGenerales[0])
-            {
-                calfMinGenerales[0]=califEst1[j][i];
-            }
-            if (califEst1[j][i]>calfMaxGenenerales[0])
-            {
-                calfMaxGenenerales[0]=califEst1[j][i];
-            }
-        }   
-    }
+    CalcularMinMax(califEst1, califMinEst1, califMaxEst1,califMinGenerales,califMaxGenenerales);
 
 
     ImprimirResultados( estudiantes, 0, materias, califEst1, 
                         promMateriasEst1, promGenerales, califMinEst1, 
-                        califMaxEst1, calfMinGenerales,calfMaxGenenerales);
+                        califMaxEst1, califMinGenerales,califMaxGenenerales);
     
 
     return 0;
@@ -167,8 +140,6 @@ void RegistrarCalificaciones(   float califEst[NUM_CALIF][NUM_MATERIAS], int num
             }
         }
     }
-
-
 }
 
 
@@ -177,8 +148,6 @@ void CalcularPromedios( float promGeneral[NUM_ESTUDIANTES], int numEst,
                         float promMateriasEst[NUM_MATERIAS],
                         float califEst[NUM_CALIF][NUM_MATERIAS])
 {
-
-    //----------------------CÁLCULO DE PROMEDIOS
     for (int i = 0; i < NUM_MATERIAS; i++)
     {
         for (int j = 0; j < NUM_CALIF; j++)
@@ -192,6 +161,45 @@ void CalcularPromedios( float promGeneral[NUM_ESTUDIANTES], int numEst,
     }
 
     promGeneral[numEst] = promGeneral[numEst]/NUM_MATERIAS;
+}
+
+
+
+void CalcularMinMax(    float califEst[NUM_CALIF][NUM_MATERIAS],
+                        float califMinEst[NUM_MATERIAS],
+                        float califMaxEst[NUM_MATERIAS],
+                        float calfMinGeneral[NUM_ESTUDIANTES],
+                        float calfMaxGeneral[NUM_ESTUDIANTES])
+{
+    for (int i = 0; i < NUM_MATERIAS; i++)
+    {
+        for (int j = 0; j < NUM_CALIF; j++)
+        {
+            if (califEst[j][i]<califMinEst[i])
+            {
+                califMinEst[i]=califEst[j][i];
+            }
+            if (califEst[j][i]>califMaxEst[i])
+            {
+                califMaxEst[i]=califEst[j][i];
+            }
+        }       
+    }
+
+    for (int i = 0; i < NUM_MATERIAS; i++)
+    {
+        for (int j = 0; j < NUM_CALIF; j++)
+        {
+            if (califEst[j][i]<calfMinGeneral[0])
+            {
+                calfMinGeneral[0]=califEst[j][i];
+            }
+            if (califEst[j][i]>calfMaxGeneral[0])
+            {
+                calfMaxGeneral[0]=califEst[j][i];
+            }
+        }   
+    }
 
 
 }
